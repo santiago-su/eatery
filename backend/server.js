d56a9f8464546a7d4ac8be308fa378f6588483d7
santiago-su <santiago.su.mo@gmail.com>
2017-04-28 18:10:58 -0500

    Add Webpack config, and hot module replacement

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const host = process.env.APP_HOST || 'localhost';
+const isProduction = process.env.NODE_ENV === 'production';
+const port = isProduction ? 8080 : 3000;
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer();
+const publicPath = path.resolve(__dirname, '..', 'public');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// Proxy to webpack-dev-server
+app.all(['/assets/*', '*.hot-update.json'], function (req, res) {
+  proxy.web(req, res, {
+    target: `http://${host}:3001`
+  });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use(express.static(publicPath));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+
+proxy.on('error', function(e) {
+  console.log('Proxy error', e);
+});
+
+app.listen(port, function () {
+  console.log('Server running on port ' + port);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
+const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const host = process.env.APP_HOST || 'localhost';
+
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].[contenthash].css',
+  disable: process.env.NODE_ENV === 'development'
+});
 
 const config = {
-  entry: path.resolve(__dirname, 'frontend', 'main.js'),
+  devtool: 'source-map',
+  entry: [
+    'webpack/hot/dev-server',
+    `webpack-dev-server/client?http://${host}:3001`,
+    path.resolve(__dirname, 'frontend', 'main.js')
+  ],
   output: {
     path: path.resolve(__dirname, 'public', 'assets'),
     filename: 'bundle.js'
@@ -10,16 +23,29 @@ const config = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['env']
           }
         }
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader', options: { sourceMap: true }
+          }, {
+            loader: 'sass-loader', options: { sourceMap: true }
+          }],
+          // use style-loader in development
+          fallback: 'style-loader'
+        })
       }
     ]
-  }
+  },
+  plugins: [extractSass, new webpack.HotModuleReplacementPlugin()]
 };
 
 module.exports = config;
