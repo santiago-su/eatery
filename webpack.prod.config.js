92146419dc9c5555b009911c92b7ce93512fc0f1
santiago-su <santiago.su.mo@gmail.com>
2017-04-29 17:31:13 -0500

    Production ready?

---
+++ webpack.prod.config.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const host = process.env.APP_HOST || 'localhost';
+
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].[contenthash].css',
+  disable: process.env.NODE_ENV === 'development'
+});
+
+const config = {
+  devtool: 'source-map',
+  entry: [
+    path.resolve(__dirname, 'frontend', 'main.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/assets'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader', options: { sourceMap: true }
+          }, {
+            loader: 'sass-loader', options: { sourceMap: true }
+          }],
+          // use style-loader in development
+          fallback: 'style-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    extractSass,
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
+};
+
+module.exports = config;
