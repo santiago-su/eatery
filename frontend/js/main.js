fe5e00581e9791bfb10b46544291237e4ef6fd13
santiago-su <santiago.su.mo@gmail.com>
2017-05-01 20:07:31 -0500

    Add gulp to project and finally get it working with webpack

---
+++ frontend/js/main.js
@@ -0,0 +1,18 @@
+import angular from 'angular';
+
+import appConfig  from './config/app.config';
+import 'angular-ui-router';
+import './config/app.templates';
+
+import './layout';
+import './home';
+
+const requires = ['ui.router', 'templates', 'app.layout', 'app.home'];
+
+window.app = angular.module('app', requires);
+
+angular.module('app').config(appConfig);
+
+angular.bootstrap(document, ['app'], {
+  strictDi: false
+});
