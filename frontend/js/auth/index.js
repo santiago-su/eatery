05f953671fc67b382aebe2cc3f340e6166dde10d
santiago-su <santiago.su.mo@gmail.com>
2017-05-04 12:51:36 -0500

    Register and login on angular

---
+++ frontend/js/auth/index.js
@@ -0,0 +1,12 @@
+import angular from 'angular';
+import AuthCtrl from './auth.controller';
+import AuthConfig from './auth.config';
+
+let authModule = angular.module('app.auth', []);
+
+authModule.controller('AuthCtrl', AuthCtrl);
+
+authModule.config(AuthConfig);
+
+
+export default authModule;
--- frontend/js/services/index.js
+++ frontend/js/services/index.js
@@ -1,9 +1,13 @@
 import angular from 'angular';
+import UserService from './user.service';
+import JwtService from './jwt.service';
 import Geolocation from './geolocation.service';
 import Restaurants from './restaurants.service';
 
 let servicesModule = angular.module('app.services', []);
 
+servicesModule.service('User', UserService);
+servicesModule.service('JWT', JwtService);
 servicesModule.service('Geolocation', Geolocation);
 servicesModule.service('Restaurants', Restaurants);
 
