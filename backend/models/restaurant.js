e0b0904ee293875ce4f0b43b17defc4e6830b0b2
santiago-su <santiago.su.mo@gmail.com>
2017-05-04 13:36:55 -0500

    First idea of restaurant full schema with reviews

--- backend/models/restaurant.js
+++ backend/models/restaurant.js
@@ -5,7 +5,12 @@ let RestaurantSchema = new Schema({
   place_id: { type: String, required: true, unique: true },
   id: { type: String, required: true, unique: true },
   name: { type: String, required: true, unique: true },
-  vicinity: { type: String, required: true, unique: true }
+  vicinity: { type: String, required: true, unique: true },
+  reviews: [{
+    score: Number,
+    author: String,
+    description: String
+  }]
 });
 
 module.exports = mongoose.model('restaurant', RestaurantSchema);
