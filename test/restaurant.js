bacd6a59994c23af6d5a84d692b7a6bf2d6c320d
santiago-su <santiago.su.mo@gmail.com>
2017-05-04 20:00:42 -0500

    Test post review route

--- test/restaurant.js
+++ test/restaurant.js
@@ -2,14 +2,16 @@
 process.env.NODE_ENV = 'test';
 
 const mongoose = require('mongoose');
+const User = require('../backend/models/user');
 const Restaurant = require('../backend/models/restaurant');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../backend/server');
 const should = chai.should();
+const chaiJWT = require('chai-jwt');
 
 chai.use(chaiHttp);
-
+chai.use(chaiJWT);
 
 describe('Restaurants', () => {
 
@@ -90,12 +92,46 @@ describe('Restaurants', () => {
         .query({ id: restaurants[0].id })
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.rshould.be.a('object');
-          res.body.restaurant.place_id.should.be.eql(restaurants[0].place_id);
-          res.body.restaurant.id.should.be.eql(restaurants[0].id);
-          res.body.restaurant.name.should.be.eql(restaurants[0].name);
-          res.body.restaurant.vicinity.should.be.eql(restaurants[0].vicinity);
+          res.body.should.be.a('object');
+          res.body.place_id.should.be.eql(restaurants[0].place_id);
+          res.body.id.should.be.eql(restaurants[0].id);
+          res.body.name.should.be.eql(restaurants[0].name);
+          res.body.vicinity.should.be.eql(restaurants[0].vicinity);
 
+          done();
+        });
+    });
+
+
+  });
+
+
+  /*
+  * POST route
+  */
+  describe('/POST restaurant/review', () => {
+
+    // it should auth a user to be able to POST to this route
+    let user = new User({
+      email: 'example',
+      password: 'secret'
+    });
+    user.save();
+    let token = user.generateJwt();
+
+    it('it should post a review from a restaurant', (done) => {
+      Restaurant.insertMany(restaurants);
+      chai.request(server)
+        .post('/api/restaurant/review')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ restaurant: { review: 'amazing restaurant' }, id: restaurants[0].id })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Success!');
+          res.body.restaurant.reviews.length.should.not.be.eql(0);
+          res.body.restaurant.reviews[0].description.should.be.eql('amazing restaurant');
+          res.body.restaurant.reviews[0].author.should.be.eql(user.email);
           done();
         });
     });
