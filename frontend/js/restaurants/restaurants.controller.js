b2b5de1ad833685730a0328f00ffe7abcee5ae2f
santiago-su <santiago.su.mo@gmail.com>
2017-05-04 20:01:43 -0500

    Setup reviews on restaurants angular side

--- frontend/js/restaurants/restaurants.controller.js
+++ frontend/js/restaurants/restaurants.controller.js
@@ -1,10 +1,24 @@
 class RestaurantsCtrl {
-  constructor(Restaurants) {
+  constructor(Restaurants, $state) {
     'ngInject';
 
     this.restaurants = Restaurants;
+    this._$state = $state;
 
+  }
 
+  postReview() {
+    this.isSubmitting = true;
+    this.restaurants.postReview(this.formData, this.restaurants.restaurant.id).then(
+      (res) => {
+        this.isSubmitting = false;
+        this._$state.reload();
+      },
+      (err) => {
+        this.isSubmitting = false;
+        this.errors = err.data.errors;
+      }
+    );
   }
 }
 
